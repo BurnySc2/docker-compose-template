7824c44525cd1a585022273b61b9a9c282181181
BurnySc2 <gamingburny@gmail.com>
2021-08-29 20:51:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+Access docker gui (portainer):
+http://localhost:9000
+
+Access postgres through pgadmin:
+http://localhost:5050
+
+Start with:
+```
+docker-compose up --build
+```
+
+docker-compose exec -it containername
---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+import os
+from typing import Optional
+
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.engine import URL
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+from loguru import logger
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+
+def main():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    host_name = "postgres" if os.environ.get("IS_INSIDE_DOCKER") else "localhost"
+    url = URL.create(drivername="postgresql", username="postgres", password="changeme", host=host_name, port=5432)
+    engine = create_engine(url)
+    for i in range(5):
+        try:
+            SQLModel.metadata.create_all(engine)
+            break
+        except OperationalError:
+            logger.error("Is postgres database running?")
+            time.sleep(2)
+
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        session.commit()
+
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        hero = session.exec(statement).first()
+        logger.info(hero)
+
+
+if __name__ == '__main__':
+    main()
